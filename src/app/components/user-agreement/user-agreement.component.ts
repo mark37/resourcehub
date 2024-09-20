de087ed8ac2f1405cb1d9502983ab9a0fb1b9a21
Mark Santos <christian.santos37@gmail.com>
2024-09-20 08:46:00 +0800

    package installations

---
+++ src/app/components/user-agreement/user-agreement.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-agreement',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-agreement.component.html',
+  styleUrl: './user-agreement.component.scss'
+})
+export class UserAgreementComponent {
+
+}
