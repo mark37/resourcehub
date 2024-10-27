623d934a32c2cb3216f56b294e746a4251560ee0
Mark Santos <christian.santos37@gmail.com>
2024-10-27 18:11:31 +0800

    approve and reject applicant

--- src/app/modules/admin/modules/dashboard/components/applicant-list/applicant-list.component.ts
+++ src/app/modules/admin/modules/dashboard/components/applicant-list/applicant-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { HttpService } from '../../../../../../shared/http.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faArrowLeft, faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faCircleNotch, faFilter } from '@fortawesome/free-solid-svg-icons';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -14,25 +14,30 @@ export class ApplicantListComponent implements OnInit {
   @Input() selected_posting!: any;
   faArrowLeft = faArrowLeft;
   faCircleNotch = faCircleNotch;
-
+  faFilter = faFilter;
   applicantList: any[] = [];
 
   isLoading: boolean = false;
   meta!: any;
   per_page: number = 10;
 
+  application_status!: string | null;
+  statusList: string[] = ['pending', 'approved', 'rejected'];
+
   loadApplicants(page?: number){
     this.isLoading = true;
     let params = {
       posting_id: this.selected_posting.id,
       show_list: 1,
-      per_page: this.per_page
-    }
+      per_page: this.per_page,
+      application_status: this.application_status
+    };
+
     this.http.get('posting-application', { params }).subscribe({
       next: (data: any) => {
         this.applicantList = data.data;
         this.meta = data.meta;
-        console.log(this.meta)
+        // console.log(this.applicantList)
         this.isLoading = false;
       },
       error: err => console.log(err)
@@ -41,6 +46,27 @@ export class ApplicantListComponent implements OnInit {
 
   closeComponent() {
     this.toggleModal.emit();
+  }
+
+  modals: any = [];
+  selected_application!: any;
+  toggleMyModal(name: string, data?: any) {
+    this.selected_application = data;
+    this.modals[name] = !this.modals[name];
+  }
+
+  updating_status: boolean = false;
+  updateApplicationStatus(is_approved: boolean) {
+    this.updating_status = true;
+    this.selected_application['is_approved'] = is_approved;
+    this.http.update('posting-application/', this.selected_application.id, this.selected_application).subscribe({
+      next: (data: any) => {
+        console.log(data);
+        this.updating_status = false;
+        this.toggleMyModal('user-details');
+      },
+      error: err => console.log(err)
+    });
   }
 
   constructor (
