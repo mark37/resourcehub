9ed7ddb0161eb00dbfe354310160e93e792a65f3
Mark Santos <christian.santos37@gmail.com>
2024-11-03 19:37:30 +0800

    update apply now buttons

--- src/app/modules/student-page/modules/scholarship/components/apply-now/apply-now.component.ts
+++ src/app/modules/student-page/modules/scholarship/components/apply-now/apply-now.component.ts
@@ -1,20 +1,82 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faFileLines, faSave } from '@fortawesome/free-solid-svg-icons';
+import { HttpService } from '../../../../../../shared/http.service';
+import { CommonModule, formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-apply-now',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './apply-now.component.html',
   styleUrl: './apply-now.component.scss'
 })
 export class ApplyNowComponent {
   @Output() toggleModal = new EventEmitter<any>();
+  @Input() selected_posting!: any;
+  @Input() isAuthenticated!: boolean;
 
+  faSave = faSave;
+  faFileLines = faFileLines;
   faArrowLeft = faArrowLeft;
+
+  showConfirmation: boolean = false;
+
+  savePost() {
+    let params: any = {
+      posting_id: this.selected_posting.id
+    };
+
+    this.http.post('posting-application', params).subscribe({
+      next: (data: any) => {
+        this.closeModal();
+      },
+      error: err => console.log(err)
+    })
+  }
+
+  toggleConfirmation() {
+    this.showConfirmation = !this.showConfirmation;
+  }
+
+  onSubmit(applied?: boolean){
+    let params: any = {};
+    params['posting_id'] = this.selected_posting.id
+
+    if(applied) params['is_applied'] = 1; params['date_applied'] = formatDate(new Date(), 'yyyy-MM-dd', 'en', 'Asia/manila');
+
+    this.http.post('posting-application', params).subscribe({
+      next: () => {
+        this.closeModal();
+      },
+      error: err => console.log(err)
+    });
+  }
+
+  applyPost() {
+    let params: any = {
+      posting_id: this.selected_posting.id,
+      is_applied: 1,
+      date_applied: formatDate(new Date(), 'yyyy-MM-dd', 'en', 'Asia/manila')
+    };
+
+    this.http.post('posting-application', params).subscribe({
+      next: (data: any) => {
+        this.closeModal();
+      },
+      error: err => console.log(err)
+    })
+  }
 
   closeModal() {
     this.toggleModal.emit();
+  }
+
+  constructor(
+    private http: HttpService
+  ) { }
+
+  ngOnInit(): void {
+    // this.htmlContent = this.sanitizer.bypassSecurityTrustHtml(this.selected_posting.description);
   }
 }
