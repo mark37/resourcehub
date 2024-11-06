dabd29f9ce9f598218471ff160391d7a7e495a07
Mark Santos <christian.santos37@gmail.com>
2024-11-06 16:28:24 +0800

    update api route

--- src/app/shared/http.service.ts
+++ src/app/shared/http.service.ts
@@ -5,7 +5,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class HttpService {
-  baseUrl = 'http://127.0.0.1:8000/api/';
+  // baseUrl = 'http://127.0.0.1:8000/api/';
+  baseUrl = 'https://api.resourcehub.cloud';
 
   get(loc: string, data?: any) {
     return this.http.get(`${this.baseUrl}` + 'v1/' +loc, data ? data : '')
