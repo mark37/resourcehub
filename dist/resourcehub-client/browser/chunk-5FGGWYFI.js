1e31cbaf9b79f1bdb56f388ceee96709cda89473
Mark Santos <christian.santos37@gmail.com>
2024-11-08 14:21:13 +0800

    build

--- dist/resourcehub-client/browser/chunk-5FGGWYFI.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-VNPCTRMG.js";import"./chunk-IEDQEACJ.js";import"./chunk-GAL4ENT6.js";export{a as AdminModule};
---
+++ dist/resourcehub-client/browser/chunk-TGIA7335.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-YQQLAVN4.js";import"./chunk-IEDQEACJ.js";import"./chunk-GAL4ENT6.js";export{a as AdminModule};
