0e5a9ad9faac2bc1d864c1a8cb63b216b8f25af7
Mark Santos <christian.santos37@gmail.com>
2024-11-08 16:01:58 +0800

    update home page

--- src/app/modules/admin/modules/home/home.component.ts
+++ src/app/modules/admin/modules/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faFilter, faPenToSquare, faTableColumns } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faArrowUpRightFromSquare, faCircleNotch, faFilter, faPenToSquare, faTableColumns } from '@fortawesome/free-solid-svg-icons';
 import { HttpService } from '../../../../shared/http.service';
 import { Router } from '@angular/router';
 
@@ -12,39 +12,76 @@ export class HomeComponent implements OnInit{
   faPenToSquare = faPenToSquare;
   faTableColumns = faTableColumns;
   faFilter = faFilter;
-
-  category: any[] = [
-    { id: 1, title: 'Verified'},
-    { id: 2, title: 'Pending'},
-    { id: 3, title: 'Applied'},
-  ];
-
-  scholarPartTime: any[] = [
-    { id: 1, title: 'Scholarship'},
-    { id: 2, title: 'Part-time Jobs'}
-  ];
+  faArrowUpRightFromSquare = faArrowUpRightFromSquare;
+  faArrowLeft = faArrowLeft;
+  faCircleNotch = faCircleNotch;
 
   userList: any[] = [];
 
   isLoading: boolean = false;
-  loadList(page?: number) {
-    this.isLoading = true;
-    let params: any = {};
-    params['page'] = page ?? 1;
+  modals: any = [];
+  selected_user!: any;
+  updating_user: boolean = false;
 
-    this.http.get('user-information').subscribe({
+  loading_draft_post: boolean = false;
+  draft_posts!: any;
+  loadDraftPost() {
+    this.loading_draft_post = true;
+
+    let params: any = {};
+    params['per_page'] = 'all';
+    params['is_published'] = 'unpublished';
+
+    this.http.get('posting-information', { params }).subscribe({
       next: (data: any) => {
         console.log(data);
-
-        this.userList = data.data;
+        this.draft_posts = data.data;
         this.isLoading = false;
       },
       error: err => console.log(err)
     })
   }
 
+  loadList(page?: number) {
+    this.isLoading = true;
+    let params: any = {};
+    params['per_page'] = 'all';
+    params['user_info'] = 1;
+    params['is_verified'] = 'pending';
+
+    this.http.get('user-information', { params }).subscribe({
+      next: (data: any) => {
+        this.userList = data.data;
+        this.loadDraftPost();
+      },
+      error: err => console.log(err)
+    })
+  }
+
+  toggleModal(name: string, data?: any) {
+    this.selected_user = data;
+    this.modals[name] = !this.modals[name];
+
+    if(!this.modals['user-details']) this.loadList();
+  }
+
   navigateTo(location: string) {
     this.router.navigate([location]);
+  }
+
+  verifyUser(value: boolean) {
+    this.updating_user = true;
+    this.selected_user['user_verified'] = value === true ? 1 : 0 ;
+    this.selected_user['is_active'] = value === true ? 1 : 0 ;
+
+    this.http.update('user-information/', this.selected_user.id, this.selected_user).subscribe({
+      next: (data: any) => {
+        console.log(this.selected_user);
+        this.updating_user = false;
+        this.toggleModal('user-details')
+      },
+      error: err => console.log(err)
+    })
   }
 
   constructor (
--- src/app/modules/admin/modules/home/home.module.ts
+++ src/app/modules/admin/modules/home/home.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UserInformationComponent } from '../../../../components/user-information/user-information.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    UserInformationComponent
   ]
 })
 export class HomeModule { }
